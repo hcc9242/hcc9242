901796a58690f738c193a33cc916b8bcc4926f18
hcc9242 <hcc_9242@rti.org.tw>
2026-05-19 11:09:37 +0800

    修正base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/hcc9242/',
   plugins: [vue()],
   build: {
     minify: "terser",
